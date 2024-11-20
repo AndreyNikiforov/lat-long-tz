5404e4405d74595e7c88214e82b3715846c47e35
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-11-20 12:16:09 -0800

    add responsive progress bar

--- gen2.py
+++ gen2.py
@@ -20,11 +20,13 @@ def get_tz(lng, lat):
 dummy_lst = [{'lng': 1.2, 'lat': 3.4, 'tz': "abc"}]
 dummy_tbl = pa.Table.from_pylist(dummy_lst)
 
+t = tqdm(total=len(lng_range) * len(lat_range) * 10**4)
+
 # parq with new tbl per row was killed/died after ~1hr, generated huge file (compression is post processing?)
 with pq.ParquetWriter('data2.parq', dummy_tbl.schema) as writer:
     #  header
     # file.write("lng,lat,tz\n")
-    for lng in tqdm([lng * 1.0 for lng in lng_range]):
+    for lng in [lng * 1.0 for lng in lng_range]:
         lst = []
         for lat in [lat * 1.0 for lat in lat_range]:
             for lng_degree1 in [degree for degree in range(0,9)]:
@@ -38,5 +40,7 @@ with pq.ParquetWriter('data2.parq', dummy_tbl.schema) as writer:
                                 tz = get_tz(lng_, lat_)
                                 if tz:
                                     lst.append({'lng': lng_, 'lat': lat_, 'tz': tz})
+                            t.update()
         tbl = pa.Table.from_pylist(lst)
         writer.write_table(tbl)
+t.close()
