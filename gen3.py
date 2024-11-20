a86d0044452226be904612429167523b98417b74
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-11-20 13:06:07 -0800

    replace tqdm with rich

--- gen3.py
+++ gen3.py
@@ -1,13 +1,10 @@
 # this will generate all lng/lat/tz tuples for training
-# pip install timezonefinder pyarrow tqdm
-
-from multiprocessing import freeze_support
-freeze_support()  # fmt: skip # fixing tqdm on macos
+# pip install timezonefinder pyarrow rich
 
 from timezonefinder import TimezoneFinder
 import pyarrow.parquet as pq
 import pyarrow as pa
-from tqdm import tqdm 
+from rich.progress import Progress, TextColumn, BarColumn, TaskProgressColumn, TimeElapsedColumn, TimeRemainingColumn,TransferSpeedColumn
 
 tf = TimezoneFinder(in_memory=True)
 
@@ -23,29 +20,36 @@ def get_tz(lng, lat):
 dummy_lst = [{'lng': 1.2, 'lat': 3.4, 'tz': "abc"}]
 dummy_tbl = pa.Table.from_pylist(dummy_lst)
 
-t = tqdm(total=len(lng_range) * len(lat_range) * 10**6)
+with Progress(
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TaskProgressColumn(),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+        TransferSpeedColumn()
+    ) as progress:
+    t = progress.add_task("Total", total=len(lng_range) * len(lat_range) * 10**6)
 
-# parq with new tbl per row was killed/died after ~1hr, generated huge file (compression is post processing?)
-with pq.ParquetWriter('data3.parq', dummy_tbl.schema) as writer:
-    #  header
-    # file.write("lng,lat,tz\n")
-    for lng in [lng * 1.0 for lng in lng_range]:
-        lst = []
-        for lat in [lat * 1.0 for lat in lat_range]:
-            for lng_degree1 in [degree for degree in range(0,9)]:
-                for lat_degree1 in [degree for degree in range(0,9)]:
-                    for lng_degree2 in [degree for degree in range(0,9)]:
-                        for lat_degree2 in [degree for degree in range(0,9)]:
-                            for lng_degree3 in [degree for degree in range(0,9)]:
-                                for lat_degree3 in [degree for degree in range(0,9)]:
-                                    if lng_degree3 > 0 or lat_degree3 > 0:
-                                        # prev precision degrees were already written to data{0,1,2}
-                                        lng_ = lng + lng_degree1/10**1 + lng_degree2/10**2 + lng_degree3/10**3
-                                        lat_ = lat + lat_degree1/10**1 + lat_degree2/10**2 + lat_degree3/10**3
-                                        tz = get_tz(lng_, lat_)
-                                        if tz:
-                                            lst.append({'lng': lng_, 'lat': lat_, 'tz': tz})
-                                    t.update()
-        tbl = pa.Table.from_pylist(lst)
-        writer.write_table(tbl)
-t.close()
+    # parq with new tbl per row was killed/died after ~1hr, generated huge file (compression is post processing?)
+    with pq.ParquetWriter('data3.parq', dummy_tbl.schema) as writer:
+        #  header
+        # file.write("lng,lat,tz\n")
+        for lng in [lng * 1.0 for lng in lng_range]:
+            lst = []
+            for lat in [lat * 1.0 for lat in lat_range]:
+                for lng_degree1 in [degree for degree in range(0,9)]:
+                    for lat_degree1 in [degree for degree in range(0,9)]:
+                        for lng_degree2 in [degree for degree in range(0,9)]:
+                            for lat_degree2 in [degree for degree in range(0,9)]:
+                                for lng_degree3 in [degree for degree in range(0,9)]:
+                                    for lat_degree3 in [degree for degree in range(0,9)]:
+                                        if lng_degree3 > 0 or lat_degree3 > 0:
+                                            # prev precision degrees were already written to data{0,1,2}
+                                            lng_ = lng + lng_degree1/10**1 + lng_degree2/10**2 + lng_degree3/10**3
+                                            lat_ = lat + lat_degree1/10**1 + lat_degree2/10**2 + lat_degree3/10**3
+                                            tz = get_tz(lng_, lat_)
+                                            if tz:
+                                                lst.append({'lng': lng_, 'lat': lat_, 'tz': tz})
+                                        progress.update(t, advance=1)
+            tbl = pa.Table.from_pylist(lst)
+            writer.write_table(tbl)
