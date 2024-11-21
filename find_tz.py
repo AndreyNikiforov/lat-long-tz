2a771e1d5c1e6b26178cd647fa991087b2fa8a54
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-11-21 11:41:34 -0800

    add option to specify not found tz

--- find_tz.py
+++ find_tz.py
@@ -16,11 +16,11 @@ dummy_tbl = pa.Table.from_pylist(dummy_lst)
 
 tf = TimezoneFinder(in_memory=True)
 
-def get_tz(lng, lat):
+def get_tz(lng, lat, default):
     try:
         return tf.timezone_at(lng=lng, lat=lat)
     except:
-        return ""
+        return default
 
 
 # https://stackoverflow.com/questions/77148138/how-to-iterate-and-read-one-row-at-at-time-from-multiple-parquet-files
@@ -37,17 +37,18 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 @click.argument("output_file", type=click.STRING)
 @click.argument("records_per_reading_batch", type=click.INT)
 @click.argument("records_per_writing_batch", type=click.INT)
+@click.option("--default", type=click.STRING, default="Error", show_default=True, help="What tz to use when find fails")
 
-def main(input_file, output_file, records_per_reading_batch, records_per_writing_batch):
+def main(input_file, output_file, records_per_reading_batch, records_per_writing_batch, default):
     """Finds tz for given file with lng-lat and prodices matching tz file 
 
-    INPUT_FILE is for the input parque file with lng-lat data, e.g `geo3-0.parquet`
+        INPUT_FILE is for the input parque file with lng-lat data, e.g `geo3-0.parquet`
 
-    OUTPUT_FILE is for the output parque file with lng-lat-tz data, e.g `data3-0.parquet`
+        OUTPUT_FILE is for the output parque file with lng-lat-tz data, e.g `data3-0.parquet`
 
-    RECORDS_PER_READING_BATCH is the number of records to read, e.g. 65536
+        RECORDS_PER_READING_BATCH is the number of records to read, e.g. 65536
 
-    RECORDS_PER_WRITING_BATCH is the number of records to accumulate in ram before flushing to disk, e.g. 10000000
+        RECORDS_PER_WRITING_BATCH is the number of records to accumulate in ram before flushing to disk, e.g. 10000000
     """
     with Progress(
             TextColumn("[progress.description]{task.description}"),
@@ -72,7 +73,7 @@ def main(input_file, output_file, records_per_reading_batch, records_per_writing
                     for rec in sub:
                         lng = rec['lng']
                         lat = rec['lat']
-                        tz = get_tz(lng, lat)
+                        tz = get_tz(lng, lat, default)
                         lst.append({'tz': tz})
                         progress.update(total_task, advance=1)
                     tbl = pa.Table.from_pylist(lst)
