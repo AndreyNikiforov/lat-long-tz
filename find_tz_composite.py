3a3a97217daaab5f09792bc5c1d5cdd6460fb4bc
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-11-23 22:14:23 +0000

    use default for missing tz in composite version

--- find_tz_composite.py
+++ find_tz_composite.py
@@ -17,11 +17,11 @@ dummy_tbl = pa.Table.from_pylist(dummy_lst)
 
 tf = TimezoneFinder(in_memory=True)
 
-def get_tz(lng, lat):
+def get_tz(lng, lat, default):
     try:
         return tf.timezone_at(lng=lng, lat=lat)
     except:
-        return ""
+        return default
 
 
 # https://stackoverflow.com/questions/77148138/how-to-iterate-and-read-one-row-at-at-time-from-multiple-parquet-files
@@ -38,8 +38,9 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 @click.argument("output_file", type=click.STRING)
 @click.argument("records_per_reading_batch", type=click.INT)
 @click.argument("records_per_writing_batch", type=click.INT)
+@click.option("--default", type=click.STRING, default="Error", show_default=True, help="What tz to use when find fails")
 
-def main(input_file, output_file, records_per_reading_batch, records_per_writing_batch):
+def main(input_file, output_file, records_per_reading_batch, records_per_writing_batch, default):
     """Finds tz for given file with lng-lat and prodices composite lng-lat-tz file
 
     INPUT_FILE is for the input parque file with lng-lat data, e.g `geo3-0.parquet`
@@ -64,20 +65,19 @@ def main(input_file, output_file, records_per_reading_batch, records_per_writing
             total_count = parquet_file.scan_contents(batch_size=records_per_reading_batch)
             progress.update(scan_task, advance=1)
             print(f"Total records: {total_count}")
-            total_task = progress.add_task("Producing lng-lat-tz file", total=total_count)
-            src = file_iterator(parquet_file, records_per_reading_batch)
+            src = progress.track(file_iterator(parquet_file, records_per_reading_batch), total=total_count)
             with pq.ParquetWriter(output_file, dummy_tbl.schema) as writer:
                 # chunk to limit ram use
                 for sub in ichunked(src, records_per_writing_batch):
-                    lst = []
+                    lst = {'lng': [], 'lat': [], 'tz': []}
                     for rec in sub:
                         lng = rec['lng']
                         lat = rec['lat']
-                        tz = get_tz(lng, lat)
-                        if tz:
-                            lst.append({'lng': lng, 'lat': lat, 'tz': tz})
-                        progress.update(total_task, advance=1)
-                    tbl = pa.Table.from_pylist(lst)
+                        tz = get_tz(lng, lat, default)
+                        lst['lng'].append(lng)
+                        lst['lat'].append(lat)
+                        lst['tz'].append(tz)
+                    tbl = pa.Table.from_pydict(lst)
                     writer.write_table(tbl)
     return 0
 
