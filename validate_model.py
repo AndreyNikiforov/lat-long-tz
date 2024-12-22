1fe1d7501e29387f3ffed8c53f8290ad63d5e258
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-12-22 09:13:49 -0800

    add benchmark to model validation

--- validate_model.py
+++ validate_model.py
@@ -5,6 +5,7 @@ import click
 import sys
 from timezonefinder import TimezoneFinder
 from rich.progress import Progress, TextColumn, BarColumn, TaskProgressColumn, TimeElapsedColumn, TimeRemainingColumn,TransferSpeedColumn
+import time
 
 tf = TimezoneFinder(in_memory=True)
 
@@ -49,16 +50,28 @@ def main(model, limit, input_name):
             TransferSpeedColumn()
         ) as progress:
         task_id = progress.add_task("Working...", total=limit)
+        total_time_model = 0
+        total_time_code = 0
         for lat, lng in sliced:
+            start_time = time.perf_counter()
             outputs = ort_sess.run(None, {input_name: [[lng, lat]]})
+            end_time = time.perf_counter()
+            total_time_model += (end_time - start_time)
+
             pred_tz = outputs[0][0][0]
+
+            start_time = time.perf_counter()
             true_tz = get_tz(lng, lat, "Error")
+            end_time = time.perf_counter()
+            total_time_code += (end_time - start_time)
+
             counter = counter + 1
             if pred_tz == true_tz:
                 correct_count = correct_count + 1
             acc = correct_count/counter
             progress.update(task_id, completed=counter, description=f"Accuracy:{acc:.1%}")
-
+        progress.print(f"Speed of the model inference: {counter/total_time_model:.1f} op/sec, and pure code: {counter/total_time_code:.1f} op/sec")
+        progress.print(f"Accuracy of the model inference: {correct_count/counter:.1%}")
     # todo progress
     return 0
 
