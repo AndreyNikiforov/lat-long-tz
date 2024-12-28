13480e86ae877a65c21c7480d39b58ed3b72a4ed
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-12-28 11:50:22 -0800

    add accuracy calc for NAM

--- validate_model.py
+++ validate_model.py
@@ -6,7 +6,7 @@ import sys
 from timezonefinder import TimezoneFinder
 from rich.progress import Progress, TextColumn, BarColumn, TaskProgressColumn, TimeElapsedColumn, TimeRemainingColumn,TransferSpeedColumn
 import time
-
+# pip install click timezonefinder rich onnxruntime
 tf = TimezoneFinder(in_memory=True)
 
 def gen_lat_long():
@@ -20,6 +20,18 @@ def get_tz(lng, lat, default):
         return tf.timezone_at(lng=lng, lat=lat)
     except:
         return default
+
+
+# NAM y,x == lat,long
+# left most 65.576472, -168.483307
+# top most 71.349914, -156.705964
+# bottom most 24.666625, -80.768467
+# right most 47.264050, -52.116124
+def get_region(lat, lng):
+    if 24.7 < lat and lat < 65.6 and \
+        -168.5 < lng and lng < -52.1:
+        return "NAM"
+    return "Other"
 
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
@@ -35,7 +47,6 @@ def main(model, limit, input_name):
         MODEL onnx or ort file of the model
 
     """
-    
 
     ort_sess = ort.InferenceSession(model)
     sliced = islice(gen_lat_long(), limit)
@@ -52,6 +63,11 @@ def main(model, limit, input_name):
         task_id = progress.add_task("Working...", total=limit)
         total_time_model = 0
         total_time_code = 0
+        regions = {'NAM': {
+            'counter': 0,
+            'correct': 0,
+        }}
+
         for lat, lng in sliced:
             start_time = time.perf_counter()
             outputs = ort_sess.run(None, {input_name: [[lng, lat]]})
@@ -66,13 +82,22 @@ def main(model, limit, input_name):
             total_time_code += (end_time - start_time)
 
             counter = counter + 1
+            region = get_region(lat, lng)
+            if region in regions:
+                regions[region]['counter'] += 1
             if pred_tz == true_tz:
                 correct_count = correct_count + 1
+                if region in regions:
+                    regions[region]['correct'] += 1
             acc = correct_count/counter
             progress.update(task_id, completed=counter, description=f"Accuracy:{acc:.1%}")
         progress.print(f"Speed of the model inference: {counter/total_time_model:.1f} op/sec, and pure code: {counter/total_time_code:.1f} op/sec")
         progress.print(f"Accuracy of the model inference: {correct_count/counter:.1%}")
-    # todo progress
+        for region in regions:
+            if regions[region]['counter'] > 0:
+                progress.print(f"Accuracy of the model inference for {region}: {regions[region]['correct']/regions[region]['counter']:.1%}")
+            else:
+                progress.print(f"No samples for {region}")
     return 0
 
 
